a9bb7b62c240e0015860f0e7a4ae076f1da7d492
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-06-30 23:45:08 -0400

    improved it

--- Easy/javas.js
+++ Easy/javas.js
@@ -1,4 +1,12 @@
-var name1 = "Darwin";
-var name2 = "Ana";
+var name1 = window.prompt("Type Name 1: ");
+var name2 = window.prompt("Type Name 2: ");
 
-console.log(`The name ${name1} is longer than ${name2} by 3 characters`);
+if (name1.length > name2.length) {
+    window.alert(`The name ${name1} is longer than ${name2} 
+    by ${name1.length - name2.length} characters`);
+}
+else{
+    window.alert(`The name ${name2} is longer than ${name1} 
+    by ${name2.length - name1.length} characters`);
+}
+
